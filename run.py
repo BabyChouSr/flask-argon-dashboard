0e61ee804fc67f7dde46e03c3625536b6b2cd559
BabyChouSr <emu4yolife@gmail.com>
2020-11-29 10:05:38 -0800

    frozen

--- run.py
+++ run.py
@@ -3,5 +3,13 @@
 License: MIT
 Copyright (c) 2019 - present AppSeed.us
 """
-
+'''
 from app import app, db
+'''
+from flask_frozen import Freezer
+from app import app,db
+
+freezer = Freezer(app)
+
+if __name__ == '__main__':
+    freezer.freeze()
